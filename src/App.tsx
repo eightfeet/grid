1db7eebe1d0a3108ef7ee3da74edaefbf5bf5e1a
eightfeet <bajiao000000@163.com>
2020-11-24 11:17:28 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import React from "react";
-import "react-grid-layout/css/styles.css";
-import "react-resizable/css/styles.css";
+import React from 'react';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
 import './App.scss';
-import EditorDemo from './EditorDemo'
+import EditorDemo from './EditorDemo';
+
+const data = [
+  {
+    layout: { i: "a", x: 1, y: 0, w: 10, h: 2 },
+    config: {name: '按钮'}
+  },
+  {
+    layout: { i: "b", x: 1, y: 0, w: 10, h: 2},
+    config: {name: '抽奖'}
+  },
+  {
+    layout: { i: "c", x: 1, y: 0, w: 10, h: 2},
+    config: {name: '报名'}
+  }
+]
 
 function App(props: any) {
-
-  return (
-    <div className="App">
-      <EditorDemo />
-    </div>
-  );
+    return (
+        <div className="App">
+            <EditorDemo
+                isEditing={false}
+                rowHeight={20}
+                cols={12}
+                width={window.innerWidth}
+                data={data}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/EditorDemo/index.tsx
+++ src/EditorDemo/index.tsx
@@ -6,75 +6,110 @@ import "react-resizable/css/styles.css";
 import s from "./test.module.scss";
 import GridLine from "./../GridLine";
 
-function EditorDemo(props: any) {
-  const [state, setstate] = useState(0);
-  const [fixed, setfixed] = useState(true);
+interface EditorProps {
+  /**
+   * 页面是否正在编辑
+   * @type {boolean}
+   * @memberof EditorProps
+   */
+  isEditing?: boolean;
+  /**
+   * 单行高度
+   * @type {number}
+   * @memberof EditorProps
+   */
+  rowHeight: number;
+  /**
+   * 布局列数
+   * @type {number}
+   * @memberof EditorProps
+   */
+  cols: number;
+  /**
+   * 宽度
+   * @type {number}
+   * @memberof EditorProps
+   */
+  width?: number;
+  /**
+   * 高度
+   * @type {number}
+   * @memberof EditorProps
+   */
+  height?: number;
+  /**
+   * 编辑器数据
+   * @type {{
+   *     layout: GridLayout.Layout;
+   *     config: any
+   *   }[]}
+   * @memberof EditorProps
+   */
+  data: {
+    layout: GridLayout.Layout;
+    config: any
+  }[]
+}
+
+/**
+ * 编辑器
+ *
+ * @param {EditorProps} { isEditing, rowHeight, cols, width, height, data}
+ * @return {*} 
+ */
+function EditorDemo({ isEditing, rowHeight, cols, width, height, data}: EditorProps) {
+  const [wrapWidth, setWrapWidth] = useState(0);
   const [wrapHeight, setWrapHeight] = useState(0);
-  const rowHeight = 20;
-  const cols = 12;
   const ref = useRef(null);
   useEffect(() => {
-    setstate(window.innerWidth);
-    setTimeout(() => {
-      setfixed(false);
-    }, 5000);
-  }, []);
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2, static: fixed },
-    { i: "b", x: 1, y: 0, w: 10, h: 2, static: fixed },
-    { i: "c", x: 11, y: 0, w: 1, h: 2, static: fixed },
-  ];
+    setSize()
+  }, [width, height]);
 
-  const onDragStop: GridLayout.ItemCallback = (
-    layout: GridLayout.Layout[],
-    oldItem: GridLayout.Layout,
-    newItem: GridLayout.Layout,
-    placeholder: GridLayout.Layout,
-    event: MouseEvent,
-    element: HTMLElement
-  ) => {
-    console.log("data");
-    console.log("layout", layout);
-    console.log("oldItem", oldItem);
-    console.log("newItem", newItem);
-    console.log("placeholder", placeholder);
-    console.log("event", event);
-    console.log("element", element.getClientRects()[0]);
-  };
+  const setSize = () => {
+    let height = 0;
+    let width = 0;
+    if (ref !== null) {
+      if (!ref.current) {
+        return
+      }
+      height = (ref.current as any).scrollHeight;
+      width = (ref.current as any).scrollWidth; 
+      setWrapHeight(height);
+      setWrapWidth(width);
+    }
+  }
 
   const onLayoutChange = () => {
-    console.log("onchange");
+    setTimeout(() => {
+      setSize()
+    });
   };
 
-  if (state === 0) {
-    return null;
-  }
   return (
     <div className={s.layout} ref={ref}>
-      {!fixed ? (
+      {isEditing ? (
         <GridLine
           width={window.innerWidth}
           cols={cols}
           rowHeight={rowHeight}
-          height={(ref.current as any).scrollHeight}
+          height={wrapHeight}
           space={10}
         />
       ) : null}
       <GridLayout
-        onDragStop={onDragStop}
         onLayoutChange={onLayoutChange}
-        layout={layout}
         cols={cols}
         rowHeight={rowHeight}
-        width={state}
+        width={wrapWidth}
         autoSize
       >
-        {layout.map((item) => (
+        {data.map(({layout, config}) => (
           <div
-            className={classNames(s.block, fixed ? null : s.modify)}
-            key={item.i}
+            className={classNames(s.block, !isEditing ? null : s.modify)}
+            key={layout.i}
+            data-grid={{...layout, static: !isEditing}}
           >
-            <div className={item.i === "b" ? s.blockcon : ""}>{item.i}</div>
+            <div className={layout.i === "b" ? s.blockcon : ""}>{layout.i}{JSON.stringify(config)}</div>
           </div>
         ))}
       </GridLayout>
