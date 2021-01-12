e918a83441e0977d2c80146b25ec51f340cb4c1d
eightfeet <bajiao000000@163.com>
2021-01-13 04:25:43 +0800

    update

--- src/components/MiniDashboard/Tabs/Tabs.tsx
+++ src/components/MiniDashboard/Tabs/Tabs.tsx
@@ -1,62 +1,88 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { AnyObjectType, AppDataElementsTypes } from 'types/appData';
-import key from 'lodash/keys.js';
-import s from './Tabs.module.scss';
+import React, { useCallback, useEffect, useState } from "react";
+import { AnyObjectType, AppDataElementsTypes } from "types/appData";
+import key from "lodash/keys.js";
+import get from "lodash/get.js";
+import s from "./Tabs.module.scss";
 
 interface Props extends AppDataElementsTypes {
-    onClick?: () => void;
+  onClick?: () => void;
 }
 
 const Tabs: React.FC<Props> = (props) => {
-    const { moduleId, onClick, type, ...other } = props;
-    const [menu, setmenu] = useState<string[]>([]);
-    const [secondmenu, setsecondmenu] = useState<string[]>([]);
-    const [current, setcurrent] = useState(null);
-    const [secondcurrent, setsecondcurrent] = useState(null);
+  const { moduleId, onClick, type, content, style, event } = props;
+  const [menuData, setmenuData] = useState<AnyObjectType[]>([]);
+  const [path, setpath] = useState<string[]>([]);
+  const [menuGod, setmenuGod] = useState<{[keys: number]: any}>({})
 
-    useEffect(() => {
-        setmenu(key(other));
-    }, []);
+  useEffect(() => {
 
-    const onClickItem = useCallback(
-        (item) => () => {
-            setcurrent(item);
-            setsecondmenu(key((props as AnyObjectType)[item]));
-            // console.log('secondmenu', (props as AnyObjectType)[item], key((props as AnyObjectType)[item]));
-            // console.log(3333, secondmenu)
-        },
-        [secondmenu]
-    );
+        // const aaa = ['a','b','c',['d','e','f',['g','h','i']]];
+        // const t = get(aaa, '[3][3][0]');
+        // console.log(t);
 
-    const onClickSecondItem = useCallback(
-        (item) => () => {
-            setcurrent(item);
-        },
-        []
-    );
+        const optData: AnyObjectType = {content, style, event}
 
-    return (
-        <>
-            <div className={s.firstnemu}>
-                {menu.map((item) => (
-                    <div
-                        className={current === item ? s.selected : ''}
-                        onClick={onClickItem(item)}
-                        key={item}
-                    >
-                        {item}
-                    </div>
-                ))}
-            </div>
-            {secondmenu.length > 0 ? (
-                <div className={s.secondmenu}>
-                    {secondmenu.map((item) => (
-                        <div>{item}</div>
-                    ))}
-                </div>
-            ) : null}
-        </>
-    );
+        const handleObj = (Obj: AnyObjectType) => {
+            const temp:any = [];
+            if (Object.prototype.toString.call(Obj) === '[object Object]') {
+                Object.keys(Obj).forEach((key, i) => {
+                    const element = Obj[key];
+                    temp.push({
+                        name: key,
+                        value: handleObj(element)
+                    });
+                    
+                })
+            }
+            return temp
+        }
+
+        setmenuGod({0:['content', 'style', 'event']})
+
+        setmenuData(handleObj(optData))
+
+  }, []);
+
+  const onClickItem = useCallback(
+    e => {
+        const nextpath = e.target.getAttribute('data-path');
+        const nextleve = parseInt(e.target.getAttribute('data-leve'));
+        const newpath = [...path];
+        newpath[nextleve] = nextpath;
+        setpath(newpath)
+        
+        const newMenuGod: any = {};
+        for (let index = 0; index < nextleve + 1; index++) {
+            newMenuGod[index] = menuGod[index];
+        }
+        newMenuGod[nextleve + 1] = get(menuData, newpath.join(''));
+        setmenuGod(newMenuGod);
+
+    },
+    [path, menuGod, menuData]
+  );
+
+  // [0, ]
+  const renderMenu = () => {
+      const nodes: any[] = [];
+      Object.keys(menuGod).forEach((key: any, y) => {
+        const element = menuGod[key];
+        const eles: any[] = [];
+        if (Array.isArray(element)) {
+            element.forEach((el, x) => {
+                eles.push(<div onClick={onClickItem} data-path={`[${x}].value`} data-leve={y} key={el.name || el}>(x:{x}){el.name || el}(y:{y})</div>)
+            });
+            nodes.push(<div className={s[`stage${key}`]} key={y}>{eles}</div>)
+        }
+      })
+      return nodes;
+  }
+
+  return (
+    <>
+        {renderMenu()}
+    </>
+  );
 };
 
 export default Tabs;
