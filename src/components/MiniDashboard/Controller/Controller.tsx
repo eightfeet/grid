b8426f5e29f1e86ff11191ed98080404946f8c48
eightfeet <bajiao000000@163.com>
2021-01-13 17:05:14 +0800

    update

--- src/components/MiniDashboard/Controller/Controller.tsx
+++ src/components/MiniDashboard/Controller/Controller.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { AllCssType } from 'types/appData';
+import React, { useCallback, useEffect, useState } from 'react';
+import { AllCssType, AnyObjectType } from 'types/appData';
+import s from './Controller.module.scss'
 
 interface Props {
     data?: AllCssType
@@ -8,7 +9,51 @@ interface Props {
 }
 
 const Controller: React.FC<Props> = ({ data }) => {
-    return <div>{JSON.stringify(data, null, 2)}</div>;
+    const [stateData, setStateData] = useState<any>();
+
+    useEffect(() => {
+        setStateData(data)
+    }, [stateData])
+
+    const onChange = useCallback(
+        (key) => () => {
+            // if (Object.prototype.toString.call(stateData) === '[object Object]') {
+            //     Object.keys(stateData).forEach(stateDataKey => {
+            //         const item = stateData[stateDataKey];
+            //         if (key === stateDataKey) {
+            //             console.log('对上了', item)
+            //         }
+            //     })
+            // }
+            
+            console.log(key)
+        },
+        [stateData],
+    )
+
+    const renderObject = (data: {} | undefined) => {
+        if (Object.prototype.toString.call(data) === '[object Object]') {
+            const result: JSX.Element[] = [];
+            const editData: AnyObjectType = data || {};
+            console.log('editData', editData)
+            Object.keys(editData).forEach(key => {
+                    const item = editData[key];
+                    if (Array.isArray(item)) {
+                        const els = item.map(el => renderObject(el))
+                        result.push(<li>{els}</li>)
+                    } else {
+                        result.push(<li><label key={key}>{key}: <input value={item} onChange={onChange(key)} /></label></li>)
+                    }
+                })
+
+
+            return <ul>{result}</ul>;
+        }
+    }
+    
+    return <div className={s.root}>
+        {renderObject(stateData)}
+    </div>;
 };
 
 export default Controller;
