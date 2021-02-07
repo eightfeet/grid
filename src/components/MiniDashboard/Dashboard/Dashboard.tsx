1ee9ab492fa152d48a276dfb0c71d165afcf645d
eightfeet <bajiao000000@163.com>
2021-02-07 22:43:34 +0800

    update

--- src/components/MiniDashboard/Dashboard/Dashboard.tsx
+++ src/components/MiniDashboard/Dashboard/Dashboard.tsx
@@ -1,16 +1,21 @@
-import React, {  } from 'react';
-import Controller from './../Controller'
-import s from './Dashboard.module.scss';
+import React from "react";
+import Controller from "./../Controller";
+import s from "./Dashboard.module.scss";
+import { Menu } from 'antd';
+const { Item } = Menu;
 
-interface Props {
-}
+interface Props {}
 
-const Dashboard:React.FC<Props> = () => {
-    return (
-        <div className={s.root}>
-            <Controller />
-        </div>
-    )
-}
-  
+const Dashboard: React.FC<Props> = () => {
+  return (
+    <div className={s.root}>
+      <Menu className={s.tab}>
+          <Item key="1">基本</Item>
+          <Item key="2">xxx</Item>
+      </Menu>
+      <Controller />
+    </div>
+  );
+};
+
 export default Dashboard;
--- src/components/MiniDashboard/MiniDashboard.tsx
+++ src/components/MiniDashboard/MiniDashboard.tsx
@@ -1,16 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import LazyLoader from '~/components/LazyLoader';
 
 
 interface MiniDashboardProps {
 }
 
-// 异步模块数据隔离，不被数据影响
-
 const MiniDashboard:React.FC<MiniDashboardProps> = function MiniDashboard(props) {
-  useEffect(() => {
-    console.log('重新渲染')
-  }, [])
   return (<LazyLoader path={'components/MiniDashboard/Dashboard'}  />);
 }
 
