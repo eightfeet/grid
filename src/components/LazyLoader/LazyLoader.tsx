a864facbec0dd57e88024ca3c6b6598e3fc2f5ff
eightfeet <bajiao000000@163.com>
2021-02-07 21:03:52 +0800

    update

--- src/components/LazyLoader/LazyLoader.tsx
+++ src/components/LazyLoader/LazyLoader.tsx
@@ -1,20 +1,18 @@
-import React, {
-    Suspense,
-    lazy,
-} from 'react';
-import { AnyObjectType } from 'types/appData';
+import React, { Suspense, lazy, useMemo } from "react";
+import { AnyObjectType } from "types/appData";
 
 interface Props extends AnyObjectType {
-    path: string
+  path: string;
 }
 
-const LazyLoader: React.FC<Props> = ({path, ...other}) => {
-    const Comp = lazy(() => import(`~/${path}`));
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Comp {...other} />
-        </Suspense>
-    );
+const LazyLoader: React.FC<Props> = ({ path, ...other }) => {
+  // Memo缓存组件防止反复刷新
+  const LazyComponent = useMemo(() => lazy(() => import(`~/${path}`)), [path]);
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LazyComponent {...other} />
+    </Suspense>
+  );
 };
 
 export default LazyLoader;
